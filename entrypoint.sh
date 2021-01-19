24de5775af9be03cc147c9af916bef5073ce64a6
Scriptless <14013314+scriptlessatwork@users.noreply.github.com>
2021-01-19 16:12:49 +0100

    Removed --nojline and nogui
    Removed --nojline and nogui to allow console output

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ apk add tzdata
 cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 apk del tzdata
 # Start the java process
-/opt/java/openjdk/bin/java -jar -Xms$MEMORYSIZE -Xmx$MEMORYSIZE $JAVAFLAGS /data/${JARFILE} --nojline nogui
+/opt/java/openjdk/bin/java -jar -Xms$MEMORYSIZE -Xmx$MEMORYSIZE $JAVAFLAGS /data/${JARFILE}
 status=$?
 if [ $status -ne 0 ]; then
   echo "Failed to start java -jar: $status"
